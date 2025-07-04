ba6c688752e8b893b4607992c144a985403f37a2
RachidJedata <jedatarachid@gmail.com>
2025-07-05 00:08:48 +0100

    feat:Lazy loading Paginated Data

--- python-generators-0x00/2-lazy_paginate.py
+++ python-generators-0x00/2-lazy_paginate.py
@@ -1,22 +1,22 @@
 import pymysql
 
-def  paginate_users(page_size, offset):
-    connection_params = {
-        "host": "localhost",
-        "user": "root",    
-        "password": "", 
-        "database": "ALX_prodev",
-        "cursorclass": pymysql.cursors.SSCursor  # Enables streaming
-    }
-
-    connection = pymysql.connect(**connection_params)
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT * FROM user_data LIMIT %s OFFSET %s",(page_size,offset))
-            for row in cursor:
-                yield row  # 🔁 Yield each row one at a time
-    finally:
-        connection.close()
 
 def lazy_paginate(page_size):
+    def  paginate_users(page_size, offset):
+        connection_params = {
+            "host": "localhost",
+            "user": "root",    
+            "password": "", 
+            "database": "ALX_prodev",
+            "cursorclass": pymysql.cursors.SSCursor  # Enables streaming
+        }
+
+        connection = pymysql.connect(**connection_params)
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM user_data LIMIT %s OFFSET %s",(page_size,offset))
+                for row in cursor:
+                    yield row  # 🔁 Yield each row one at a time
+        finally:
+            connection.close()
     return paginate_users(page_size=page_size,offset=0)
