a7052ccecaeb015d629ed3be951bfc502eee16b2
RachidJedata <jedatarachid@gmail.com>
2025-07-11 18:51:14 +0100

    Logging database Queries

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,28 @@
+import sqlite3
+import functools
+
+#### decorator to lof SQL queries
+
+""" YOUR CODE GOES HERE"""
+def log_queries(func):
+     @functools.wraps(func)
+     def wrapper(*args, **kwargs):
+        query = kwargs.get('query') if 'query' in kwargs else (args[0] if args else '')
+        print(f"[LOG] Executing SQL Query: {query}")
+        result = func(*args, **kwargs)
+        print(f"[LOG] Query returned {len(result)} rows.")
+        return result
+    
+     return wrapper
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
