56fc4d10b98176974f8fe3a9ef72bd0ef5fa4a6b
RachidJedata <jedatarachid@gmail.com>
2025-07-18 16:17:58 +0100

    Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,10 +14,20 @@ def access_nested_map(nested_map: Mapping, path: Sequence) -> Any:
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    # @parameterized.expand([
+    #     ({"a": 1}, ("a",), 1),
+    #     ({"a": {"b": 2}}, ("a","b"), 2),
+    #     ({"a": {"b": 2}}, ("a", "b"), 2),
+    # ])
+    
     @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a","b"), 2),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
+  
+    def test_access_nested_map_exception(self, nested_map, path):
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), f"'{path[-1]}'")    
