3d1b39a3c25c2b9808038c590f098232abef71db
RachidJedata <jedatarachid@gmail.com>
2025-07-24 18:56:35 +0100

    Add Permissions

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -9,7 +9,7 @@ from .permissions import IsParticipantOfConversation,IsMessageParticipant
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
-    permission_classes = [IsParticipantOfConversation]
+    permission_classes = [permissions.IsAuthenticated, IsParticipantOfConversation]
 
     filter_backends = [filters.SearchFilter, DjangoFilterBackend]
     search_fields = ['participants__username']
@@ -39,11 +39,15 @@ class ConversationViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(conversation)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+    def get_queryset(self):
+        return Message.objects.filter(conversation__participants=self.request.user)
+    
+
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
-    permission_classes = [IsMessageParticipant]
+    permission_classes = [permissions.IsAuthenticated, IsMessageParticipant]
 
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['conversation']  # filter messages by conversation ID
