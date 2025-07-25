9bfd879863fff07388275175aea2c8c7fa42c5f1
RachidJedata <jedatarachid@gmail.com>
2025-07-25 16:15:04 +0100

    Enforce chat user Role Permissions

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -69,7 +69,7 @@ class OffensiveLanguageMiddleware:
             ip = request.META.get('REMOTE_ADDR')
         return ip
     
-class RolePermissionMiddleware:
+class RolepermissionMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
