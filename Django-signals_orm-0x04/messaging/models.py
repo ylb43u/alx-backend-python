09327756655a28c234b427ec49478dde24736e8d
RachidJedata <jedatarachid@gmail.com>
2025-07-30 16:00:51 +0100

    Implement Signals for User Notifications

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -7,7 +7,7 @@ class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_messages")
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="received_messages")
     content = models.TextField()
-    sent_at = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"From {self.sender} to {self.receiver} at {self.sent_at}"
