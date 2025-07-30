1af00474c08aafaed4203b3791d1cd18da6ae5e1
RachidJedata <jedatarachid@gmail.com>
2025-07-30 17:04:13 +0100

    Leverage Advanced ORM Techniques for Threaded Conversations

--- Django-signals_orm-0x04/Django-Chat/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'messaging_app.apps.MessagingConfig'
--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -6,11 +6,18 @@ class Message(models.Model):
     message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, db_index=True)
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_messages")
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="received_messages")
+    parent_message = models.ForeignKey(
+        'self',
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='replies'
+    )
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f"From {self.sender} to {self.receiver} at {self.sent_at}"
+def __str__(self):
+        return f"{self.sender} ➝ {self.receiver} ({'Reply' if self.parent_message else 'Message'})"
     
 class Notification(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notifications")
