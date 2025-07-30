16f5a3f98b4b1b27f44a1b79ee73a5d672a4da91
RachidJedata <jedatarachid@gmail.com>
2025-07-30 16:06:05 +0100

    Implement Signals for User Notifications

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -11,3 +11,12 @@ class Message(models.Model):
 
     def __str__(self):
         return f"From {self.sender} to {self.receiver} at {self.sent_at}"
+    
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notifications")
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name="notifications")
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"🔔 Notification for {self.user.username} - Message from {self.message.sender.username}"
--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,9 +1,13 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from . models import Message
+from . models import Message, Notification
 
 @receiver(post_save,sender=Message)
 def message_created_handler(sender,instance,created,**kwargs):
     if created:
         print(f"✅ New message sent from {instance.sender} to {instance.receiver} at {instance.sent_at}")
+        Notification.objects.create(
+            user=instance.receiver,
+            message=instance
+        )
         # You can send notifications, emails, or update other models here.
